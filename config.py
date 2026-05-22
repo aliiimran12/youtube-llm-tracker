f1cf7973aae751aa96ca85e0dcba5aa086bc0803
Ali Imran <25131898d@connect.polyu.hk>
2026-05-22 16:00:06 +0500

    Add real summaries for available videos

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 YT_APIKEY = os.environ.get("YT_APIKEY", "AIzaSyA7kE8jee3x55TonmeiU-ZPdYLijju9Mho")
-GEMINI_APIKEY = os.environ.get("GEMINI_APIKEY", "AIzaSyBTLWHSUSn3qtaVACornbxYZTYIGhohlrA")
+GEMINI_APIKEY = os.environ.get("GEMINI_APIKEY", "AIzaSyCuP_Z30Gy5Ow4T1W3V22bh7KF0o5dFy0U")
 
 CHANNELS = [
     ("UCXUPKJO5MZQN11PqgIvyuvQ", "Andrej Karpathy"),
